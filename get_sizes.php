8e23a13ab5f0ea9d4d24fe379090504a5a38ebb2
char <32-kot@mail.ru>
2020-12-02 10:25:31 +0300

    catch fetch error

--- get_sizes.php
+++ get_sizes.php
@@ -1,10 +1,10 @@
 <?php
 try {
     require_once __DIR__.'/db.php';
+	$stmt = $connection->query('SELECT * FROM sizes');
+	if (!$stmt)
+	    die('db fetch fali');
+	echo json_encode($stmt->fetchAll());
 } catch (PDOException $e) {
     die();
 }
-$stmt = $connection->query('SELECT * FROM sizes');
-if (!$stmt)
-    die('db fetch fali');
-echo json_encode($stmt->fetchAll());
